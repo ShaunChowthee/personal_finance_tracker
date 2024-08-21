26f9520ea8b7c1887ccb439a0557f5f62c794ab3
Shaun Chowthee <sn.chowthee@gmail.com>
2024-08-21 04:34:32 +0200

    updating transactions.py

--- finance_tracker/transactions.py
+++ finance_tracker/transactions.py
@@ -15,7 +15,13 @@ def add_transaction(file_path):
   transactions.append(transaction)
   write_transactions(file_path, transactions)
 
+def view_transactions(file_path):
+  transactions = read_transactions(file_path)
+  for line in transactions:
+    print(line)
 
 if __name__ == "__main__":
   file_path = "../data/transactions.json"
-  add_transaction(file_path)
+  #add_transaction(file_path)
+  view_transactions(file_path)
+
