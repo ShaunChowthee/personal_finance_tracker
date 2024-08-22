2fbc41caa45d38d8aa2e658062942a847bfbf77f
Shaun Chowthee <sn.chowthee@gmail.com>
2024-08-22 02:02:25 +0200

    adding calculations

--- finance_tracker/calculations.py
+++ finance_tracker/calculations.py
@@ -3,3 +3,10 @@ from file_handler import read_transactions
 def total_expenses(file_path):
   transactions = read_transactions(file_path)
   print(sum(expense.get("amount") for expense in transactions))
+
+def total_income(file_path):
+  transactions = read_transactions(file_path)
+  print(sum(expense.get("amount") for expense in transactions if expense.get("category") == "Income"))
+
+
+total_income("../data/transactions.json")
