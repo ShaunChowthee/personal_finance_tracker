2db8204b8fe9499cfe02ed0db9e5405534c15742
Shaun Chowthee <sn.chowthee@gmail.com>
2024-08-22 02:47:13 +0200

    initialized main.py with basic functionnalities

--- finance_tracker/calculations.py
+++ finance_tracker/calculations.py
@@ -1,12 +1,9 @@
-from file_handler import read_transactions
+from .file_handler import read_transactions
 
 def total_expenses(file_path):
   transactions = read_transactions(file_path)
-  print(sum(expense.get("amount") for expense in transactions))
+  return sum(expense.get("amount") for expense in transactions)
 
 def total_income(file_path):
   transactions = read_transactions(file_path)
-  print(sum(expense.get("amount") for expense in transactions if expense.get("category") == "Income"))
-
-
-total_income("../data/transactions.json")
+  return sum(expense.get("amount") for expense in transactions if expense.get("category") == "Income")
